5f4fcf9ca1356992a8bd2de6272a8bce1434ddc4
Damiano <belluccidamiano@gmail.com>
2021-02-06 23:52:49 +0100

    init form for adding questions to db

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import InputComponent from "./inputComponent";
-
+import Form from "./Form";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <InputComponent></InputComponent>
+        {false && <InputComponent></InputComponent>}
+        <Form></Form>
       </header>
     </div>
   );
---
+++ src/Form.tsx
@@ -0,0 +1,12 @@
+import { JsxElement } from "typescript";
+
+interface interfaceForm {}
+export default (props: interfaceForm) => {
+  return (
+    <>
+      <form>
+        <input></input>
+      </form>
+    </>
+  );
+};
