81559fdfd46f34d03e3d1bbb007abc475448fba6
Damiano <belluccidamiano@gmail.com>
2021-02-08 22:17:38 +0100

    inizialization of hook for new chapter

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,20 +1,19 @@
-import axios from "axios";
+import React from "react";
 
-interface interfaceForm {}
-export default (props: interfaceForm) => {
+const Form = (props: any) => {
+  const newChapter = React.useRef<HTMLInputElement>(null);
+
+  const createChapter = (el: any) => {
+    alert(el.current.value);
+  };
+
   return (
     <>
       <form>
-        <input placeholder="Insert chapter"></input>
-        <button
-          onClick={async (event) => {
-            event.preventDefault();
-            alert("request");
-          }}
-        >
-          Save
-        </button>
+        <input type="text" ref={newChapter} placeholder="Insert chapter" />
+        <button onClick={() => createChapter(newChapter)}>Send</button>
       </form>
     </>
   );
 };
+export default Form;
