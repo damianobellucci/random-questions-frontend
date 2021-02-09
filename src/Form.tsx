ab826b61a1366908c8becfcfb47206065eb5436d
Damiano <belluccidamiano@gmail.com>
2021-02-09 23:07:06 +0100

    init prototype of insert and show chapters

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = (props: any) => {
   const newChapter = React.useRef<HTMLInputElement>(null);
+  const [showAddChapter, updateShowAddChapter] = useState(false);
 
   const createChapter = (el: any) => {
     alert(el.current.value);
@@ -9,10 +10,33 @@ const Form = (props: any) => {
 
   return (
     <>
-      <form>
-        <input type="text" ref={newChapter} placeholder="Insert chapter" />
-        <button onClick={() => createChapter(newChapter)}>Send</button>
-      </form>
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          updateShowAddChapter(true);
+        }}
+      >
+        Add chapter
+      </button>
+      {showAddChapter && (
+        <form>
+          <input type="text" ref={newChapter} placeholder="Insert chapter" />
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              createChapter(newChapter);
+              updateShowAddChapter(false);
+            }}
+          >
+            Send
+          </button>
+        </form>
+      )}
+      <ul>
+        {["chapter1", "chapter2", "chapter3"].map((el, idx) => {
+          return <li>{el}</li>;
+        })}
+      </ul>
     </>
   );
 };
